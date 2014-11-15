f220fa0ce63abc73b0e259ee73ddba634ad670ae
SamDivine <samdivinejch@gmail.com>
2014-11-15 10:32:09 +0800

    added auto refresh to webui

--- webui/routes/index.js
+++ webui/routes/index.js
@@ -21,20 +21,22 @@ module.exports = function(app) {
 function translate(mStr) {
 	var rst = mStr;
 	var prefix = mStr.substr(0, mStr.indexOf('_'));
+	var replaceStr;
 	switch(prefix) {
 	case 'pttest':
-		rst = rst.replace(prefix, '指针跟踪');
+		replaceStr = '指针跟踪';
 		break;
 	case 'bfs':
-		rst = rst.replace(prefix, '宽度优先搜索');
+		replaceStr = '宽度优先搜索';
 		break;
 	case 'wordcount':
-		rst = rst.replace(prefix, '基于Hash的查找');
+		replaceStr = '基于Hash的查找';
 		break;
 	default:
-		rst = rst.replace(prefix, '测试程序');
+		replaceStr = '测试程序';
 		break;
 	}
+	rst = rst.replace(prefix, replaceStr);
 	return rst;
 };
 
@@ -45,7 +47,7 @@ index = function(req, res){
 running = function(req, res) {
 	db.all("SELECT * FROM RUNNING_TBL ORDER BY STARTTIME ASC", function(err, rows) {
 		req.listtype = 'running';
-		if (!rows) {
+		if (rows.length === 0) {
 			console.log("empty running result");
 			res.render('list', { 
 				tasklist: []
@@ -63,44 +65,53 @@ running = function(req, res) {
 
 tasktrack = function(req, res) {
 	mTask = req.params.task;
-	var cmd = "SELECT * FROM TASKTRACK_TBL WHERE PROGRAM = '" + mTask + "' ORDER BY ATIME ASC";
-	db.all(cmd, function(err, rows) {
-		var mTitle = translate(mTask);
-		if (!rows) {
-			console.log("task " + mTask + " not found");
-			res.render('tasktrack', {
-				title: mTitle,
-				exectime: [],
-				workers: [],
-				tasks: [],
-				switches: []
-			});
+	var cmd = "SELECT * FROM RUNNING_TBL WHERE PROGRAM = '" + mTask + "'";
+
+	db.all(cmd, function(err, runnings) {
+		if (runnings.length === 0) {
+			req.listtype = 'finished';
 		} else {
-			mExectime = new Array();
-			mWorkers = new Array();
-			mTasks = new Array();
-			mSwitches = new Array();
-			for (i = 0; i < rows.length; i++) {
-				mExectime[i] = rows[i].EXECTIME;
-				mWorkers[i] = rows[i].WORKERS;
-				mTasks[i] = rows[i].FINISHEDTASKS;
-				mSwitches[i] = rows[i].CONTEXTSWITCHES;
-			}
-			res.render('tasktrack', {
-				title: mTitle,
-				exectime: mExectime,
-				workers: mWorkers,
-				tasks: mTasks,
-				switches: mSwitches
-			});
+			req.listtype = 'running';
 		}
+		cmd = "SELECT * FROM TASKTRACK_TBL WHERE PROGRAM = '" + mTask + "' ORDER BY ATIME ASC";
+		db.all(cmd, function(err, rows) {
+			var mTitle = translate(mTask);
+			if (rows.length === 0) {
+				console.log("task " + mTask + " not found");
+				res.render('tasktrack', {
+					title: mTitle,
+					exectime: [],
+					workers: [],
+					tasks: [],
+					switches: []
+				});
+			} else {	
+				mExectime = new Array();
+				mWorkers = new Array();
+				mTasks = new Array();
+				mSwitches = new Array();
+				for (i = 0; i < rows.length; i++) {
+					mExectime[i] = rows[i].EXECTIME;
+					mWorkers[i] = rows[i].WORKERS;
+					mTasks[i] = rows[i].FINISHEDTASKS;
+					mSwitches[i] = rows[i].CONTEXTSWITCHES;
+				}
+				res.render('tasktrack', {
+					title: mTitle,
+					exectime: mExectime,
+					workers: mWorkers,
+					tasks: mTasks,
+					switches: mSwitches
+				});
+			}
+		});
 	});
 };
 
 finished = function (req, res) {
-	db.all("SELECt * FROM FINISHED_TBL ORDER BY FINISHTIME ASC", function(err, rows) {
+	db.all("SELECT * FROM FINISHED_TBL ORDER BY FINISHTIME ASC", function(err, rows) {
 		req.listtype = 'finished';
-		if (!rows) {
+		if (rows.length === 0) {
 			console.log("empty finished result");
 			res.render('list', {
 				tasklist: []
